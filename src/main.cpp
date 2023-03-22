c8878af82b4c504b54d3d063b83c956c8427d44e
Amin Kamal <abmak93@gmail.com>
2023-03-23 01:19:05 +0300

    pretty print

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,16 @@ void Print(const TrieNode<Entry> *root)
 
   if (root->is_end_marker)
   {
-    // TODO: last name
-    std::cout << root->value << " , [PHONE]:" << root->entry_.phone_number << std::endl;
+    std::string last_name = "";
+
+    if (root->entry_.name_reference != nullptr)
+      last_name = root->entry_.name_reference->value;
+
+    std::cout
+        << root->value
+        << "\t\t" << last_name
+        << "\t\t" << root->entry_.phone_number
+        << std::endl;
   }
 }
 
@@ -28,15 +36,22 @@ int main(int argc, char **argv)
   auto last_name_ref = root_lname->Insert("cena");
 
   first_name_ref->SetEntry({last_name_ref, "0795555555"});
-  last_name_ref->SetEntry({first_name_ref, "0795555555"});
+  last_name_ref->SetEntry({first_name_ref});
 
   root_fname->Insert("johanna");
 
   std::cout << "Contents of first names trie:" << std::endl;
+  std::cout << "First name\tLast name\tPhone" << std::endl;
   Print(root_fname);
 
+  std::cout << std::endl;
   std::cout << "Contents of last names trie:" << std::endl;
+  std::cout << "First name\tLast name\tPhone" << std::endl;
   Print(root_lname);
+
+
+
+  std::cout << std::endl;
 
   auto first_name = root_fname->FindExact("john");
   auto last_name = root_lname->FindExact("cena");
